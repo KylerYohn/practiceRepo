3475c719971e7dfdea5cdfe65b57ef62bf739c98
Kyler Yohn <kyyohn@gmail.com>
2019-03-09 13:26:02 -0500

    fourth

--- Java.js
+++ Java.js
@@ -17,17 +17,7 @@ var QueryUrl = "https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key
 // });
 
 
-//=============================IGDB CALL
-// $.ajax({
-//     type: 'GET',
-//     url: "http://api-endpoint.igdb.com/games/1942?fields=*",
-//     headers: {
-//         'user-key': '89267c056e5105fb4d4fd944625d0641',
-//         'Accept': 'application/json'
-//     }
-// }).done(function (response) {
-//     console.log(response);
-// });
+
 
 
 
@@ -43,32 +33,47 @@ var QueryUrl = "https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key
 // });
 
 //=======================================================GIANT BOMB API (works sorta)
+$.ajax({
+    url: "https://www.giantbomb.com/api/user_reviews/?api_key=6d4d56add8ea2c13803b154586967c3fc3973982 ",
+    method:"GET"
+}).then(function(response){
+    console.log(response);
+})
+
+// var apikey = "nwpnm8fp3yqn8ataswweg546";
+// var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";
+
+// // construct the uri with our apikey
+// var moviesSearchUrl = baseUrl + '/movies.json?apikey=' + apikey;
+// var query = "Gone with the Wind";
+
+// $(document).ready(function() {
+
+//   // send off the query
+//   $.ajax({
+//     url: moviesSearchUrl + '&q=' + query,
+//     method: "GET",
+//     // success: searchCallback
+//   }).then(function(response){
+//       console.log(response);
+//   })
+// });
+
+$.ajax({
+  url: "https://api.themoviedb.org/3/discover/movie?api_key=e03904a8c30bca382b1315442046ab16",
+  method: "GET"
+}).then(function(response){
+  console.log(response);
+})
+
+//=============================IGDB CALL
 // $.ajax({
-//     url: "https://www.giantbomb.com/api/user_reviews/?api_key=6d4d56add8ea2c13803b154586967c3fc3973982&limit=10",
-//     method:"GET"
-// }).then(function(response){
+//     type: 'GET',
+//     url: "http://api-endpoint.igdb.com/games/1942?fields=*",
+//     headers: {
+//         'user-key': '89267c056e5105fb4d4fd944625d0641',
+//         'Accept': 'application/json'
+//     }
+// }).done(function (response) {
 //     console.log(response);
-// })
-
-var apikey = "nwpnm8fp3yqn8ataswweg546";
-var baseUrl = "https://api.rottentomatoes.com/api/public/v1.0";
-
-// construct the uri with our apikey
-var moviesSearchUrl = baseUrl + '/movies.json?apikey=' + apikey;
-var query = "Gone with the Wind";
-
-$(document).ready(function() {
-
-  // send off the query
-  $.ajax({
-    url: moviesSearchUrl + '&q=' + query,
-    method: "GET",
-    // success: searchCallback
-  }).then(function(response){
-      console.log(response);
-  })
-});
-
-// function searchCallback(results){
-//     console.log(results)
-// }
+// });
