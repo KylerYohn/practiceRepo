899f6fad9cc43df73be853b077970d2b0841a16f
Kyler Yohn <kyyohn@gmail.com>
2019-03-09 11:50:13 -0500

    second

--- Java.js
+++ Java.js
@@ -4,32 +4,71 @@
 
 var QueryUrl = "https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=D0aORjYxSYjihO8QnG86bs1yQJ5QfQpC"
 
-$.ajax({
-    url: QueryUrl,
-    method: "GET",
 
-}).then(function(response){
+//===============================TIMES CALL (Works)
+// $.ajax({
+//     url: QueryUrl,
+//     method: "GET",
 
-console.log(response);
+// }).then(function(response){
+
+// console.log(response);
     
-})
+// });
 
-var URLLL = ""
 
-$.ajax({
-    url: "https://api-v3.igdb.com/private/reviews",
-    method: "POST",
-    headers: {
-        "Accept": "application/json",
-        "user-key": "89267c056e5105fb4d4fd944625d0641",
-    }
-        }).then(function(response){
-        console.log(response.data);
-    })
+//=============================IGDB CALL
+// $.ajax({
+//     type: 'GET',
+//     url: "http://api-endpoint.igdb.com/games/1942?fields=*",
+//     headers: {
+//         'user-key': '89267c056e5105fb4d4fd944625d0641',
+//         'Accept': 'application/json'
+//     }
+// }).done(function (response) {
+//     console.log(response);
+// });
 
-$.ajax({
-    url: "http://api.rottentomatoes.com/api/public/v1.0/movies/770672122/reviews.json?apikey=dg9cs9k5wpdwk3wzwn9mmdtr",
-    method: "GET"
-}).then(function(response){
-    console.log(response);
+
+
+//=====================================================rotten tomatoes call
+//     $.ajax({
+//     url: "http://api.rottentomatoes.com/api/public/v1.0/movies/770672122/reviews.json?apikey=nwpnm8fp3yqn8ataswweg546",
+//     method: "GET",
+ 
+    
+    
+// }).then(function(response){
+//     console.log(response);
+// });
+
+//=======================================================GIANT BOMB API (works sorta)
+// $.ajax({
+//     url: "https://www.giantbomb.com/api/user_reviews/?api_key=6d4d56add8ea2c13803b154586967c3fc3973982&limit=10",
+//     method:"GET"
+// }).then(function(response){
+//     console.log(response);
+// })
+
+var apikey = "nwpnm8fp3yqn8ataswweg546";
+var baseUrl = "http://api.rottentomatoes.com/api/public/v1.0";
+
+// construct the uri with our apikey
+var moviesSearchUrl = baseUrl + '/movies.json?apikey=' + apikey;
+var query = "Gone with the Wind";
+
+$(document).ready(function() {
+
+  // send off the query
+  $.ajax({
+    url: moviesSearchUrl + '&q=' + query,
+    method: "GET",
+    // success: searchCallback
+  }).then(function(response){
+      console.log(response);
+  })
 });
+
+// function searchCallback(results){
+//     console.log(results)
+// }
